ff8c6040bce5670ba71a2c21137901da05b6c058
Marios Effraimiadis <marioseffraimiadis@mariosmacbookpro.mynet>
2018-08-25 20:26:00 +0100

    some more comments

--- Todoe/TodoListViewController.swift
+++ Todoe/TodoListViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 //subclassing
-class TodoListViewController: UITableViewController {  // if you change the name of the file to : TodoListViewController allakse kai to onoma edw.
-//we inherrited the UITableViewController and adding a Table View Controller to the story board (instead of the a normal view controller). We dont need set ourselves as the delegate or datasource or link iboutlets. Xcode takes care of all of that.
+class TodoListViewController: UITableViewController {  //If you change the name of the swift file to: TodoListViewController; then also change the class name (here).
+//We inherrited the UITableViewController. Adding a Table View Controller to the story board (instead of the a normal view controller) means that we dont need set ourselves as the delegate or datasource or link the ib-outlets. Xcode takes care of all of that.
    
     var itemArray = ["Finish IOS", "Learn IOS", "Practice IOS"] //creating a new item array
     
@@ -19,17 +19,16 @@ class TodoListViewController: UITableViewController {  // if you change the name
         super.viewDidLoad()
         
         //itemArray = defaults.array(forKey: "TodoListArray") as! [String]
-        //If you dont put the following then it is like you have saved our itemArray into are defaults (which is the file {com.effraimiadis.Todoe.plist} which you can find if you follow the result from : print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)) but we havent used are saved item to load up the tableView (!!!). It will still get all data from the itemArray (who is not yet persistent).
+        //If you dont put the following then it is like you have saved our itemArray into are defaults (which is the file {com.effraimiadis.Todoe.plist} which you can find if you follow the result from: print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)) but we havent used are saved item to load up the tableView (!!!). It will still get all data from the itemArray (who is not yet persistent).
         if let items = defaults.array(forKey: "TodoListArray") as? [String] //hard coded key. Cast as an array of strings.
         { itemArray = items
         }
     }
     
-    //MARK - Creating Tableview Datasource Methods
-    //what the tableview should display and homw many rows we should have.
+    //MARK - Creating Tableview Datasource Methods (what the tableview should display and homw many rows we should have).
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
-    //You start by writing table view. This creates 3 cells in the table view. (because we have 3 items only).
+    //You start by writing table view. This creates 3 cells in my table view. (because we have 3 items only).
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -45,34 +44,34 @@ class TodoListViewController: UITableViewController {  // if you change the name
         return cell //The cell returns to the tableView and gets displayed.
     }
     
-    //MARK - TableView Delegate Methods
-    
+    //MARK - TableView Delegate Methods (auto douleuei opote kanoume click se mia row)
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(indexPath.row) //prints row
         //print(itemArray[indexPath.row]) //prints the item array of a row
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none }
-            //if it has a checkmare we change its accessory to none.
+        //if it has a checkmark we change its accessory to none.
         else
         {tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark}
-        tableView.deselectRow(at: indexPath, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)  //so that it will not stay gray everytime we click on an array
     }
     
-//MARK - Add New Items
+    //MARK - Add New Items
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert) //creating a new alert
-        //creating a UIAlert action (the button that you are going to press when you are done with writing your new to do list item)
+            //creating a UIAlert action (the button that you are going to press when you are done with writing your new to do list item)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            //what will happen once the user clicks the Add Item Button on our UIAlert (!!!)     (this is a closure) //print(textField.text)
+            //what will happen once the user clicks the Add Item Button on our UIAlert (!!!)(this is a closure) //print(textField.text)
         
             self.itemArray.append(textField.text!) //force unwrap because it will never nil (it can be an empty string but not nill) if the user puts nothing then he will have an empty cell (because it will be an empty string)
         
-            //saving the updated item array to our user defaults. The key "TodoListArray" is going to unidentify this array inside our user defaults.
+                //saving the updated item array to our user defaults. The key "TodoListArray" is going to unidentify this array inside our user defaults.
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
-            self.tableView.reloadData() }
+            self.tableView.reloadData() } //make sure you reload (if you dont reload, it will not take into the account the new array that has the extra item (hence it will not appear.
+        
         alert.addTextField { (alertTextField) in                                                         //to add some text on the text field
             alertTextField.placeholder = "Create new Item"                           //in will show in grey and dissapear when we click on it
             textField = alertTextField }
